e313e968df226675db31ee3efba18eba5eeda98d
jokerx003 <marabhunta7@gmail.com>
2019-10-26 22:22:14 +0700

    test minus value

--- assets/js/editorialscore.js
+++ assets/js/editorialscore.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
   $('input').on('click', function(){
     var valeur = 0;
     $('input:checked').each(function(){
-      valeur += parseInt($(this).attr('value'),10);
+      valeur += parseInt($(this).attr('value'));
     });
     $('.progress-bar').css('width', valeur+'%').attr('aria-valuenow', valeur).html(valeur+'%');
   });
