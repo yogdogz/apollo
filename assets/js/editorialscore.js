52b87ba128245ba544015aefe11fcaca454ecd5c
jokerx003 <marabhunta7@gmail.com>
2019-10-26 21:51:03 +0700

    Update editorialscore checkbox

--- assets/js/editorialscore.js
+++ assets/js/editorialscore.js
@@ -3,10 +3,7 @@ $(document).ready(function() {
 $('input').on('click', function(){
   var valeur = 0;
   $('input:checked').each(function(){
-    if ( $(this).attr('value') > valeur )
-    {
-        valeur =  $(this).attr('value');
-       }
+    valeur += $(this).attr('value');
   });
   $('.progress-bar').css('width', valeur+'%').attr('aria-valuenow', valeur).html(valeur+'%');
 });
